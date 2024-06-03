7a82fcd1f366c75eb0f147bdbaaa8275d8b485fc
Ekaterina Shipunova <odegda@rtr22.ru>
2024-06-04 01:31:13 +0700

    внесла изменения в ui

--- ui.py
+++ ui.py
@@ -1,16 +1,22 @@
 from logger import *
 
 def interfase():
-    print('Добрый день! Вы попали на специальный бот справочник. \n 1 - запись данных \n 2 - вывод данных')
-    command=int(input())
+    print(f'Добрый день! Вы попали на специальный бот справочник.'
+          f'\n 1 - запись данных \n 2 - вывод данных \n 3 - изменение данных \n 4 - удаление данных')
+    command=int(input('Введите номер от 1 до 4: '))
 
-    while command !=1 and command !=2:
+    while command !=1 and command !=2 and command!=3 and command !=4:
         print('Неправильный ввод')
-        command=int(input('Введите число '))
+        command=int(input('Введите число от 1 до 4: '))
     
     if command ==1:
         input_data()
     elif command ==2:
-        print_data()    
+        print_data()   
+    elif command ==3:
+        change_data()   
+    elif command ==4:
+        del_data()        
+
 
        
