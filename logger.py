3893d7dcbb630758fe462993327649819697da28
Ekaterina Shipunova <odegda@rtr22.ru>
2024-06-03 21:47:21 +0700

    внесла изменения в logger в метод печати

--- logger.py
+++ logger.py
@@ -19,11 +19,11 @@ def input_data():
 
     if var==1:
         with open('data_first_variant.csv','a',encoding='utf-8') as f: # файл открываем в режиме 'a'-дописываем файл. Открываем как имя f
-            f.write(f'{name}\n{surname}\n{phone}\n{adress}\n\n')
+            f.write(f'{name}\n{surname}\n{phone}\n{adress}\n\n') # дописываем в файл информацию по варианту1
 
     elif var==2:
         with open('data_second_variant.csv','a',encoding='utf-8') as f: # файл открываем, как имя f
-            f.write(f'{name};{surname};{phone};{adress}\n')            
+            f.write(f'{name};{surname};{phone};{adress}\n')      # дописываем в файл информацию по варианту2      
 
 
 def print_data():
@@ -32,20 +32,30 @@ def print_data():
         data_first=f.readlines()        # читаем все наши строки. в переменную data_first
         #print(data_first) # ['name1\n', 'surname\n', 'phone\n', 'adress\n', '\n', 'name2\n', 'surname\n', 'phone\n', 'adress\n', '\n', 'ola\n', 'hj\n', '456\n', 'jhhb\n', '\n', 'lolo\n', 'kl\n', '456\n', 'kloiu\n', '\n']
         
-        data_first_list=[]              # каждую строку будем добавлять в список
-        j=0
-        for i in range(len(data_first)):
-            if data_first[i]=='\n' or i==len(data_first)-1: # i-тый элемент равен переходу на новую строку или i=последней записи
-                data_first_list.append(''.join(data_first[j:i+1])) # в список добавляем элемент. Преобразовываем список в строку с помощью функции join
-                j=i
-        print(''.join(data_first_list))   
+        # ВАРИАНТ НА СЕМИНАРЕ //////////////////////////////////////////////////////////////////////
+        # data_first_list=[]              # каждую строку будем добавлять в список
+        # j=0
+        # for i in range(len(data_first)):
+            
+        #     if data_first[i]=='\n' or i==len(data_first)-1: # i-тый элемент равен переходу на новую строку или i=последней записи
+        #         data_first_list.append(''.join(data_first[j:i+1])) # в список добавляем элемент. Преобразовываем список в строку с помощью функции join
+        #         j=i
+        #     #print(f'i={i}, data_ferst{i}={data_first[i]}, data_first_list={data_first_list}  ')    
+           
+        # print(''.join(data_first_list))   
+        
+        # МОЙ ВАРИАНТ///////////////////////////////////////////////////////////////////////////////////////////////
+        # for i in range(len(data_first)):
+        #     print(str(data_first[i]), end="")
+        print('',*data_first)
             
     
     print('Вывожу данные из 2 файла: \n')
     with open('data_second_variant.csv','r',encoding='utf-8') as f:
         data_second=f.readlines() 
-        print(*data_second)
-
+        print('',*data_second)  
+        #print(data_second)# ['name1;surname;phone;adress\n', '\n', 'name2;surname;phone;adress\n', '\n', 'kmmk;mjkm;4568;kli\n', '\n', 'длло;дльол;123456;олдлд\n', '\n', 'орпап;прнку;456987;ррол\n', '\n', 'лолд;длд;длоло;щщот\n']
+print_data()
         
     
 
